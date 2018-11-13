f5483a8f1be1aecd0a0bc447e82c688d10c6e321
Nelson Neto <nelsonp2n@gmail.com>
2018-11-13 10:17:07 -0200

    Outra forma de criar o server

--- HWServer/hello_server.js
+++ HWServer/hello_server.js
@@ -1,5 +1,19 @@
 const http = require('http');
 
+// const requisicao = (request, response) => {
+//     response.writeHead(200, {'Content-Type': 'text/html'});
+//     response.write('<h1>Hello World!</h1>');
+//     response.end();
+// }
+
+// const server = http.createServer(requisicao);
+
+// const serverOn = () => {
+//     console.log('Servidor rodando na porta 3000');
+// }
+
+// server.listen(3000, serverOn);
+
 // Criando servidor
 const server = http.createServer((request, response) => {
     // Leitura do cabeçalho. Neste caso, fará leitura de dados html
